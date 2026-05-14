9d7fb98f9867841053eed5e3cc0a1542bf365ac0
Giel Boogaerts <12102462@student.pxl.be>
2026-05-14 09:54:01 +0200

    Changes for online deployment

---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,29 @@
+import fs from 'node:fs/promises'
+
+const urls = [
+    '/',
+    '/work',
+    '/about',
+    '/projects/microtype',
+    '/projects/jumbledscript',
+    '/projects/eopa',
+    '/projects/typeinterplay',
+    '/projects/virgajesse',
+    '/projects/enzomari',
+    '/projects/archetype',
+    '/projects/phoneticalphabet'
+]
+
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls.map(url => `
+  <url>
+    <loc>https://gielboogaerts.com${url}</loc>
+  </url>
+`).join('')}
+</urlset>
+`
+
+await fs.writeFile('./dist/sitemap.xml', sitemap)
+
+console.log('✅ sitemap.xml generated')
