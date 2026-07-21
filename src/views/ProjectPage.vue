6bca6b334897dad97cd02d3b412884ec1b1fac4d
Giel Boogaerts <12102462@student.pxl.be>
2026-07-21 18:58:17 +0200

    Restructuring of the ProjectPage.vue and web optimalization of images

--- src/views/ProjectPage.vue
+++ src/views/ProjectPage.vue
@@ -1,7 +1,7 @@
 <script setup>
-import { useI18n } from 'vue-i18n'
+import {useI18n} from 'vue-i18n'
 
-const { t, locale } = useI18n()
+const {t, locale} = useI18n()
 
 defineProps({
   project: Object,
@@ -13,89 +13,70 @@ defineProps({
 <template>
   <main v-if="project">
 
-    <!-- TITLE -->
     <h1>
       <template v-for="(line, index) in project.titleLines" :key="index">
-        <span>{{ line }}</span><br v-if="index < project.titleLines.length - 1" />
+        <span>{{ line }}</span><br v-if="index < project.titleLines.length - 1"/>
       </template>
     </h1>
 
-    <!-- BACK -->
-    <RouterLink class="back" to="/work">
-      <span>{{ t('project.back') }}</span>
-    </RouterLink>
+    <p class="year">{{ project.year }}</p>
 
-    <!-- CONTENT -->
+    <div class="links">
+      <a v-if="project.downloadPdf" :href="project.downloadPdf.href" target="_blank" class="link no-underline">
+        {{ project.downloadPdf.label[locale] }}
+      </a>
+      <a v-if="project.link" :href="project.link.href" target="_blank" class="link no-underline">
+        {{ project.link.label[locale] }}
+      </a>
+      <a v-if="project.git" :href="project.git.href" target="_blank" class="link no-underline" id="github">
+        {{ project.git.label[locale] }}
+      </a>
+    </div>
+
+    <div class="tags" v-if="project.tags">
+      <div class="tag-col">
+        <p class="tag-title">{{ project.tags.medium.title[locale] }}</p>
+        <ul>
+          <li v-for="(item, i) in project.tags.medium.items" :key="i">{{ item[locale] }}</li>
+        </ul>
+      </div>
+
+      <div class="tag-col">
+        <p class="tag-title">{{ project.tags.design.title[locale] }}</p>
+        <ul>
+          <li v-for="(item, i) in project.tags.design.items" :key="i">{{ item[locale] }}</li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="description">
+      <p
+          v-for="(paragraph, index) in project.description[locale]"
+          :key="index"
+      >
+        {{ paragraph }}
+      </p>
+    </div>
+
     <section>
-
-      <!-- DESCRIPTION -->
-      <p>{{ project.description[locale] }}</p>
-
-      <!-- DYNAMIC CONTENT -->
       <template v-for="(block, index) in project.content" :key="index">
-
-        <!-- IMAGE -->
         <img
             v-if="block.type === 'image'"
             :src="block.src"
             :class="block.position"
             :alt="block.alt[locale]"
         />
-
-        <!-- CAPTION -->
         <p v-else-if="block.type === 'text'">
           {{ block[locale] }}
         </p>
-
       </template>
-
-      <!-- YEAR LAST -->
-      <p>{{ project.year }}</p>
-
-      <!-- OPTIONAL DOWNLOAD -->
-      <a
-          v-if="project.downloadPdf"
-          :href="project.downloadPdf.href"
-          target="_blank"
-          class="link"
-      >
-        {{ project.downloadPdf.label[locale] }}
-      </a>
-
-      <!-- OPTIONAL LINK -->
-      <a
-          v-if="project.link"
-          :href="project.link.href"
-          target="_blank"
-          class="link"
-      >
-        {{ project.link.label[locale] }}
-      </a>
-
-      <a
-          v-if="project.git"
-          :href="project.git.href"
-          target="_blank"
-          class="link"
-          id="github"
-      >
-        {{ project.git.label[locale] }}
-      </a>
-
     </section>
 
     <div class="rotation">
-      <RouterLink
-          v-if="prev"
-          :to="`/work/${prev.id}`"
-      >
+      <RouterLink v-if="prev" :to="`/work/${prev.id}`">
         {{ t('project.prev') }}
       </RouterLink>
-
-      <RouterLink
-          v-if="next"
-          :to="`/work/${next.id}`"
-      >
+      <RouterLink v-if="next" :to="`/work/${next.id}`">
         {{ t('project.next') }}
       </RouterLink>
     </div>
@@ -112,31 +93,32 @@ h1 {
   font: var(--headline);
   color: var(--primary-color);
   padding-left: 1rem;
-  margin-bottom: 10.5rem;
   margin-top: 0.5rem;
 }
 
 section {
   width: 100%;
-  padding: 0 5vw;
   display: flex;
   flex-direction: column;
   gap: 5vw;
 }
 
-.back {
-  width: fit-content;
-  position: relative;
-  bottom: 6rem;
-  align-items: center;
-  gap: 0.4rem;
-  margin-left: 1rem;
-  padding: 0;
-  background: none;
-  border: none;
-  cursor: pointer;
-  font: var(--header-3);
-  color: var(--dark-color);
+.year {
+  text-align: right;
+  font: var(--headline);
+  font-weight: normal;
+  color: var(--primary-color);
+}
+
+.description {
+  padding: 7rem 5vw;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+.description p {
+  text-align: left;
 }
 
 .rotation {
@@ -161,57 +143,71 @@ img {
 
 .link {
   align-self: center;
-  background: var(--primary-color);
-  color: white;
+  border: 1px solid var(--primary-color);
+  color: var(--primary-color);
   border-radius: 2rem;
   padding: 0.5rem 1rem;
 }
 
-#github {
-  margin-top: -3rem;
+.link:hover {
+  background-color: var(--primary-color);
+  color: white;
+}
+
+.links {
+  display: flex;
+  gap: 1rem;
+  padding: 1rem;
+}
+
+.tags {
+  display: flex;
+  gap: 4rem;
+  padding: 1rem;
+}
+
+.tag-col {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+}
+
+.tag-title {
+  font: var(--body-bold);
+  color: var(--dark-color);
+  text-align: left;
+  padding: 0;
+}
+
+.tag-col ul {
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
+}
+
+.tag-col li {
+  font: var(--body-regular);
+  color: var(--dark-color);
 }
 
 .first {
   margin-left: 0;
+  width: 100%;
 }
-
-.second {
-  margin-left: 40%;
-}
-
-.third {
-  margin-left: 20%;
-}
-
-.fourth {
-  margin-left: 55%;
-}
-
-.fifth {
-  margin-left: 10%;
-}
+.second { margin-left: 38%; }
+.third { margin-left: 20%; }
+.fourth { margin-left: 50%; }
+.fifth { margin-left: 10%; }
 
 @media screen and (max-width: 900px) {
-  img {
-    width: 60%;
-  }
-
-  .second {
-    margin-left: 35%;
-  }
-
-  .fourth {
-    margin-left: 10%;
-  }
+  img { width: 60%; }
+  .second { margin-left: 35%; }
+  .fourth { margin-left: 10%; }
 }
 
 @media screen and (max-width: 600px) {
-  img {
-    width: 100%;
-  }
-
-  .first, .second, .third, .fourth, .fifth {
-    margin-left: 0;
-  }
+  img { width: 100%; }
+  .first, .second, .third, .fourth, .fifth { margin-left: 0; }
 }
 </style>
